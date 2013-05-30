1e5454b531d7f6bec52ec997aba73af97d542ab2
root <root@li475-165.members.linode.com>
2013-05-30 07:30:03 +0000

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,8 @@ CTxMemPool mempool;
 unsigned int nTransactionsUpdated = 0;
 
 map<uint256, CBlockIndex*> mapBlockIndex;
-uint256 hashGenesisBlock("0x384b060671f4a93948e9c168216dadb0ca2fbc54aa11c86b0345b6af1c59b2f5");
+//uint256 hashGenesisBlock("0x384b060671f4a93948e9c168216dadb0ca2fbc54aa11c86b0345b6af1c59b2f5");
+uint256 hashGenesisBlock("0xde806d3d5feafc37b448d7e5ed8ec6100df64ec9210fb453b60892815f689ec8");
 static CBigNum bnProofOfWorkLimit(~uint256(0) >> 20); // starting difficulty is 1 / 2^12
 CBlockIndex* pindexGenesisBlock = NULL;
 int nBestHeight = -1;
@@ -2037,6 +2038,8 @@ bool LoadBlockIndex(bool fAllowNew)
 	block.nTime = 1369806552;
         block.nBits    = 0x1e0ffff0;
 //        block.nNonce   = 2085386442;
+	block.nNonce = 508737;
+
 
         if (fTestNet)
         {
@@ -2054,7 +2057,7 @@ bool LoadBlockIndex(bool fAllowNew)
 	assert(block.hashMerkleRoot == uint256("0x23b6916b1d8411930b249b062df7cdadb31b9c30951e27d8593143a42869fdb2"));
 
         // If genesis block hash does not match, then generate new genesis hash.
-        if (true && block.GetHash() != hashGenesisBlock)
+        if (false && block.GetHash() != hashGenesisBlock)
         {
             printf("Searching for genesis block...\n");
             // This will figure out a valid hash and Nonce if you're
